2a2e7bdf99d7f4f9f4ba04937a0792c7981fface
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-05-09 16:23:09 -0400

    Correction vérification role

--- src/scripts/verifyRole.ts
+++ src/scripts/verifyRole.ts
@@ -1,10 +1,16 @@
 import { userService } from '@/services/userService';
+import { useAuthStore } from '../stores/authStore'
 
 export enum Role {
     STUDENT = "student",
     TEACHER = "teacher"
 }
 
+
 export async function isGoodRole(role: Role): Promise<boolean> {
-    return await userService.isUserWithGoodRole("2", role)
+    const authStore = useAuthStore()
+  //sauthStore.loadPersistedToken()
+    let userId: string = authStore.getUserId
+    console.log(userId)
+    return await userService.isUserWithGoodRole(userId, role)
 }
