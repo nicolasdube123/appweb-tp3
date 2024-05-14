2cf7537c274ee28fde20c7cef90b43d87b4d2afb
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-05-14 13:28:17 -0400

    Implémetntation terminé du service de questions

--- src/services/questionService.ts
+++ src/services/questionService.ts
@@ -6,15 +6,20 @@ const QUESTION_PATH: string = "/questions"
 const API_URL: string = "http://127.0.0.1:3000" + QUESTION_PATH
 
 //En incluant les id déjà présent dans la bd, ceci est le premier index disponible
-let id: number = 4
+let id: number = 2
+function getQuestionId(): number {
+  let idToReturn = id
+  id++
+  return idToReturn
+}
 
-async function getQuestionById (userId: string): Promise<Question> {
+async function getQuestionById (questionId: string): Promise<Question> {
     try {
       // axiosAuth est une instance d'axios configurée pour ajouter le JWT à une requête nécessitant une authentification.
       // voir le fichier src/shared/axiosAuth.js
       const response = await axiosAuth.get(
         // TODO : utiliser une variable d'environnement pour l'url de l'api rest
-        API_URL +`/${userId}`
+        API_URL +`/${questionId}`
       )
   
       return response.data
@@ -22,7 +27,29 @@ async function getQuestionById (userId: string): Promise<Question> {
       throw parseAxiosError(error)
     }
 }
+
+async function createQuestion (question: Question) {
+  try {
+    await axiosAuth.post(API_URL, {
+      question
+    })
+  } catch (error) {
+    throw parseAxiosError(error)
+  }
+}
+
+async function deleteQuestion (questionId: string) {
+  try {
+    await axiosAuth.delete(API_URL + "/" + questionId)
+  } catch (error) {
+    throw parseAxiosError(error)
+  }
+}
+
   
 export const userService = {
-    getQuestionById
+  getQuestionId,
+  getQuestionById,
+  createQuestion,
+  deleteQuestion
 }
