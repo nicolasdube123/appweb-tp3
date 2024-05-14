48960b9381a4d253329dd950ca84d79e80679e0c
Raphaël Boudreault <raphael.boudreault@gmail.com>
2024-05-14 14:53:02 -0400

    Ajout de test dans QuestionColumn

--- src/components/teacher/__tests__/QuestionColumn.test.ts
+++ src/components/teacher/__tests__/QuestionColumn.test.ts
@@ -3,17 +3,19 @@ import { mount } from '@vue/test-utils'
 import QuestionColumn from '../QuestionColumn.vue'
 import Question from '@/interfaces/IQuestion'
 
+let question: Question = {
+    id: 1,
+    studentId: "2",
+    content: "Message",
+    super: true,
+    priority: "1",
+    category: "Action",
+    private: false,
+    open: true
+}
+
 let arrayQuestions: Array<Question> = [
-    {
-        id: 1,
-        studentId: "2",
-        content: "Message",
-        super: true,
-        priority: "1",
-        category: "Action",
-        private: false,
-        open: true
-    }
+    question
 ]
 
 describe('QuestionColumn.vue', () => {
@@ -32,5 +34,51 @@ describe('QuestionColumn.vue', () => {
         expect(buttonCategory.exists()).toBeTruthy()
         expect(buttonDeleteQuestion.exists()).toBeTruthy()
         expect(buttonToggleQuestion.exists()).toBeTruthy()
+    })
+
+    it("Lorsque aucune question n'est envoyé, les champs de question n'existent pas", async () => {
+        let wrapper = mount(QuestionColumn, {
+            propsData: { 
+                questions: []
+            }
+        })
+
+        let buttonDeleteQuestion = wrapper.find("#deleteQuestion")
+        let buttonToggleQuestion = wrapper.find("#toggleQuestion")
+
+        expect(buttonDeleteQuestion.exists()).toBeFalsy()
+        expect(buttonToggleQuestion.exists()).toBeFalsy()
+    })
+
+    it("Le composant contient la question envoyé en props", async () => {
+        let wrapper = mount(QuestionColumn, {
+            propsData: { 
+                questions: arrayQuestions
+            }
+        })
+
+        let studentId = wrapper.find("#studentId")
+        let content = wrapper.find("#content")
+        let priority = wrapper.find("#priority")
+        let category = wrapper.find("#category")
+        let isPrivate = wrapper.find("#isPrivate")
+
+        expect(studentId.text()).toContain(question.studentId)
+        expect(content.text()).toContain(question.content)
+        expect(priority.text()).toContain(question.priority)
+        expect(category.text()).toContain(question.category)
+        expect(isPrivate.text()).toContain(question.private)
+    })
+
+    it("Lorsque question supprimé, la question n'existe plus", async () => {
+        let wrapper = mount(QuestionColumn, {
+            propsData: { 
+                questions: arrayQuestions
+            }
+        })
+
+        wrapper.find("#deleteQuestion").trigger("click")
+
+        expect(wrapper.vm.$props.questions.length).toBe(0)
     })
 })
